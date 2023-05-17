724fad4c04deccd9de475111be125f6d0b55cf8d
Zygodon <j.b.pilkington@gmail.com>
2023-05-17 20:49:24 +0100

    Plots by quadrat

--- SBM by quadrat.R
+++ SBM by quadrat.R
@@ -16,7 +16,7 @@ dbDisconnectAll <- function(){
   lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
   cat(sprintf("%s connection(s) closed.\n", ile))
 }
-GetSurveyData <-  function()
+GetQuadratData <-  function()
 {
   # GET DATA FROM DB
   # Remote DB with password
@@ -28,23 +28,16 @@ GetSurveyData <-  function()
                    host   = "sxouse.ddns.net")
   
   # SQL query to extract survey data and associated species  
-  q <- sprintf('SELECT DISTINCT assembly_name, community, species_name
+  q <- sprintf('SELECT DISTINCT quadrat_id, species_name
 from surveys join quadrats on quadrats.survey_id = surveys.surveys_id
 join records on quadrats.quadrats_id = records.quadrat_id
 join species on species.species_id = records.species_id
 where species.species_id != 4 and major_nvc_community like "MG%%" and quadrat_size = "2x2";') 
   
-  # NOTE: this extract includes "MG5", i.e. some MG5 communities where 
-  # the team have not decided
-  # on a sub-latent_community.
-  
   rs1 = dbSendQuery(con, q)
   return(as_tibble(fetch(rs1, n=-1)))
   dbDisconnectAll()
 }
-
-## NewName <- function(...){paste("lc", lc, sep = "")}
-
 
 ### DATA WRANGLING #####################
 # GET DATA FROM DB
@@ -185,7 +178,7 @@ edges <- g1 %>% activate(edges) %>% as_tibble()
 plt1 <- ggplot(edges, aes(lor))  +
   geom_histogram(aes(y = ..density..), binwidth = 0.25, colour = "black") +
   geom_vline(xintercept = 0, colour = "red") +
-  xlim(-3, 6) +
+  xlim(-5, 10) +
   ylim(0.0, 0.4) +
   labs(title ="", x = "log(odds ratio)")
 plot(plt1)
@@ -201,7 +194,7 @@ the_model <- estimateSimpleSBM(M, 'bernoulli', estimOptions = list(plot = F )) #
 rm(M) # Clean up...
 
 # select which model to use (default is the "best")
-# the_model$setModel(6)
+the_model$setModel(9)
 
 # Print C matrix. Note that it can be recovered from the_model$connectParam
 print(as_tibble(the_model$connectParam))
@@ -241,22 +234,21 @@ plot(ggraph(
 ### LATENT COMMUNITY SUMMARY ###############
 # Needs the_model, g1 and possibly d
 # Import data for including sites in the analysis
-survey_data <- GetSurveyData() %>%
-  rename(site = assembly_name, species = species_name) %>%
-  select(-community) # Don't need assessed NVC
+quadrat_data <- GetQuadratData() %>%
+  rename(species = species_name)
 
-## n_sites <- survey_data %>% select(site) %>% distinct() %>% count() %>% as.integer()
+nq <- quadrat_data %>% select(quadrat_id) %>% distinct() %>% count() %>% as.integer()
 
 hits <- gather(d) %>% 
   group_by(key) %>% 
   summarise(count = sum(value)) %>% 
-  mutate(frequency = 100*count/n_sites) %>%
+  mutate(frequency = 100*count/nq) %>%
   rename(species = key)
-# count: how many sites the species was found in.
+# count: how many quadrats the species was found in.
 
-# In g1, add species name to nodes
+# In g1, add count and frequency to nodes
 g1 <- g1 %>% activate(nodes) %>% left_join(hits, join_by(name == species))
-# and to edges from - to
+# and species names to edges from - to
 g1 <- g1 %>% activate(edges) %>% mutate(A = .N()$name[from], B = .N()$name[to])
 
 ### Make lc_stats with lc_range for calculating lc_expression by site
@@ -275,29 +267,29 @@ lc_range <- function(.x) {
 lc_stats <- map_df(.x = lc_stats$lc, 
                    .f = lc_range) # So now lc_stats is ready to be used when I need to calculate site expressions.
 
-### Wrangling to calculate LC expression by site
-sv <- rep(survey_data %>% distinct(site), the_model$nbBlocks) %>% unlist() %>% unname(sv) %>% sort()
-sv <- tibble(site = sv, lc = rep(seq(1:the_model$nbBlocks), length(sv)/the_model$nbBlocks))
+### Wrangling to calculate LC expression by quadrat
+qx <- rep(quadrat_data %>% distinct(quadrat_id), the_model$nbBlocks) %>% unlist() %>% unname(qx) %>% sort()
+qx <- tibble(quadrat = qx, lc = rep(seq(1:the_model$nbBlocks), length(qx)/the_model$nbBlocks))
 
 # extract the site and LC pairs from sv as separate vectors
-sites <- sv %>% pull(site) %>% as.character
-lc <- sv %>% pull(lc)
+quadrats <- qx %>% pull(quadrat) # %>% as.character
+lc <- qx %>% pull(lc)
 
-exp_at_site <- function(.x, .y){
-  spp_to_choose_from <- survey_data %>%
-        filter(site == .x) %>%
+exp_at_quadrat <- function(.x, .y){
+  spp_to_choose_from <- quadrat_data %>%
+        filter(quadrat_id == .x) %>%
         pull(species)
   
-  sg <-  g1 %>% activate(edges) %>% # the sub-graph of g1 with dyads drawn from a site/LC combination
+  sg <- g1 %>% activate(edges) %>% # the sub-graph of g1 with dyads drawn from a quadrat/LC combination
         filter(edge_latent_community == .y) %>%
         filter((A %in% spp_to_choose_from) &  (B %in% spp_to_choose_from))
   rng <-sg %>% activate(edges) %>% as_tibble() %>% summarise(n=n())
-  expression_lc_at_site <- 100*rng$n/lc_stats$range[.y]
-  return(ifelse(is.na(expression_lc_at_site), 0, expression_lc_at_site))
+  expression_lc_at_quadrat <- 100*rng$n/lc_stats$range[.y]
+  return(ifelse(is.na(expression_lc_at_quadrat), 0, expression_lc_at_quadrat))
 }
 
-site_xp <- sv %>% mutate(xp = map2_dbl(.x = sites, .y = lc, .f = exp_at_site))
-rm(sv, n_sites, lc)
+quadrat_xp <- qx %>% mutate(xp = map2_dbl(.x = quadrats, .y = lc, .f = exp_at_quadrat))
+rm(qx, nq, lc)
 
 ### LC MESOSCOPIC PLOTS ################
 meso_plot_list <- map(.x = lc_stats$lc,
@@ -320,29 +312,30 @@ meso_plot_list <- map(.x = lc_stats$lc,
                                  theme_graph())
                         })
 
-### SITE-SPECIES BIPARTITE PLOTS #################################
-# Strategy: make a bipartite graph of all sites and species; get the sub-graph
-# for each plot. Start with survey_data
+### QUADRAT-SPECIES BIPARTITE PLOTS #################################
+# Strategy: make a bipartite graph of all quadrats and species; get the sub-graph
+# for each plot. Start with quadrat_data
 
 # Thank you Laszlo Gadar for how to make bipartite graph
 # https://rpubs.com/lgadar/load-bipartite-graph
-bp <- graph.data.frame(survey_data, directed = F)
-V(bp)$type <- V(bp)$name %in% survey_data[,2]$species #the second column of edges is TRUE type
+bp <- graph.data.frame(quadrat_data, directed = F)
+V(bp)$type <- V(bp)$name %in% quadrat_data[,2]$species #the second column of edges is TRUE type
 bp <- as_tbl_graph(bp)
-bp <- bp %>% activate(nodes) %>% mutate(kind = ifelse(type, "species", "site"))
+bp <- bp %>% activate(nodes) %>% mutate(kind = ifelse(type, "species", "quadrat"))
 
 bipartite_plot_list <- map(.x = lc_stats$lc, .f = ~{
     this_lc <- .x
     spp <- g1 %>% activate(nodes) %>% filter(latent_community == .x) %>% select(name) %>% as_tibble()
-    sg <- bp %>% activate(nodes) %>% filter(kind == "site" | name %in% spp$name)
+    sg <- bp %>% activate(nodes) %>% filter(kind == "quadrat" | name %in% spp$name)
     # remove isolated nodes
     isolates <- which(degree(sg)==0) # Not Tidygraph
     sg <- as_tbl_graph(delete.vertices(sg, isolates))
-    # Need to get the lc expressions of **this** lc over all sites - to make symbol size
+    # Need to get the lc expressions of **this** lc over all quadrats - to make symbol size
     lcxp <- map_df(.x = this_lc, .f = ~{
-      site_xp %>% filter(lc == this_lc) %>% select(site, xp)
+      quadrat_xp %>% filter(lc == this_lc) %>% select(quadrat, xp)
     })
-    sg <- sg %>% activate(nodes) %>% left_join(lcxp, join_by(name == site))
+    lcxp <- lcxp %>% mutate(q = as.character(quadrat))
+    sg <- sg %>% activate(nodes) %>% left_join(lcxp, join_by(name == q))
     sg <- sg %>% activate(nodes) %>% mutate(cb = centrality_betweenness(
         weights = NULL,
         directed = FALSE,
@@ -352,9 +345,9 @@ bipartite_plot_list <- map(.x = lc_stats$lc, .f = ~{
     plot(p2 <- sg %>% ggraph(layout = "stress") +
     geom_edge_link(colour = "grey80") +
     scale_colour_brewer(palette = "Dark2") +
-    # geom_node_point(aes(colour = fct_rev(kind), shape = fct_rev(kind), size = ifelse(kind == "site", xp, cb))) +
-    geom_node_point(aes(colour = fct_rev(kind), shape = fct_rev(kind), size = ifelse(kind == "site", xp, 3))) +
-    # geom_node_text(aes(label = ifelse(kind == "species", name, "")), colour = 'black', repel = T, size=3) +
+    # geom_node_point(aes(colour = fct_rev(kind), shape = fct_rev(kind), size = ifelse(kind == "quadrat", xp, cb))) +
+    geom_node_point(aes(colour = fct_rev(kind), shape = fct_rev(kind), size = ifelse(kind == "quadrat", xp, 3))) +
+    geom_node_text(aes(label = ifelse(kind == "species", name, "")), colour = 'black', repel = T, size=3) +
     ggtitle(paste("Latent Community", .x, sep = " ")) +
     guides(size = guide_legend(title = "Community expression %", override.aes=list(shape = 17,colour = "#d95f02")),
            shape = guide_legend(title="", override.aes=list(size = 4)),
@@ -363,31 +356,12 @@ bipartite_plot_list <- map(.x = lc_stats$lc, .f = ~{
 })
 
 # 
-### GENERAL SITE LC-EXPRESSION POLAR PLOT ###############
+### GENERAL QUADRAT LC-EXPRESSION POLAR PLOT ###############
 
-# Polar plot labels based on work by Yan Holz
-# https://r-graph-gallery.com/296-add-labels-to-circular-barplot.html?utm_content=cmp-true
-
-# Sum of LC expression for each community needed for label y-values
-label_y <- site_xp %>% select(-lc) %>% group_by(site) %>% summarise(y = sum(xp))
-y_max <- ceiling(max(label_y$y))
-
-site_labels <- site_xp %>%
-  select(site) %>%
-  distinct() %>%
-  mutate(id = seq_along(site)) %>%
-  # Subtract 0.5 because the letter must have the angle of the center of the bars,
-  # not extreme right(1) or extreme left (0)
-  mutate(angle =  90 - 360 * (id-0.5) /length(site)) %>%
-  # calculate the alignment of labels: right or left
-  # If I am on the left part of the plot, my labels have currently an angle < -90
-  mutate(hjust = ifelse( angle < -90, 1, 0)) %>%
-  # Flip angles BY 180 degrees to make them readable
-  mutate(angle=ifelse(angle < -90, angle+180, angle))
-
-site_xp <- site_xp %>% arrange(site) # Just to be sure
-p <- ggplot(site_xp) +
-  geom_col(aes(x = site, y = xp, fill = as.factor(lc))) +
+y_max <- 100
+quadrat_xp <- quadrat_xp %>% arrange(quadrat) # Just to be sure
+plot(p <- ggplot(quadrat_xp %>% filter(lc < 7)) +
+  geom_col(aes(x = quadrat, y = xp, fill = as.factor(lc))) +
   scale_fill_brewer(palette = "Accent") +
   coord_polar(start = 0) +
   ylim(-50,y_max) +
@@ -396,62 +370,62 @@ p <- ggplot(site_xp) +
     axis.title.x = element_blank(),
     axis.title.y = element_text("Latent community expression"), # ?not work
     plot.margin = unit(rep(1,4), "cm") # Adjust the margin to make sure labels are not truncated!
-  )
+  ))
 # # Add the site labels.
-plot(p + geom_text(data = site_labels, aes(x=id, y=ceiling(0.8*y_max), label=site, hjust=hjust),
-                   color="black", alpha=0.6, size=2, angle=site_labels$angle, inherit.aes = FALSE ) +
-       guides(fill = guide_legend("Latent Community")) +
-       labs(title = "Site expressions of latent communities"))
+# plot(p + geom_text(data = site_labels, aes(x=id, y=ceiling(0.8*y_max), label=site, hjust=hjust),
+#                    color="black", alpha=0.6, size=2, angle=site_labels$angle, inherit.aes = FALSE ) +
+#        guides(fill = guide_legend("Latent Community")) +
+#        labs(title = "Site expressions of latent communities"))
 
-### SITE EXPRESSIONS OF LC POLAR PLOTS ################
+### {SITE} EXPRESSIONS OF LC POLAR PLOTS - NOT IMPLEMENTED FOR QUADRATS ################ 
 
-polar_plot_list <- map(.x = lc_stats$lc, .f = ~{
-  data <- site_xp %>% filter(lc == .x)
-  data_labels <- data %>% 
-    select(site) %>% 
-    mutate(id = seq_along(site)) %>% 
-    # Subtract 0.5 because the letter must have the angle of the center of the bars, 
-    # not extreme right(1) or extreme left (0)
-    mutate(angle =  90 - 360 * (id-0.5) /length(site)) %>%  
-    # calculate the alignment of labels: right or left
-    # If I am on the left part of the plot, my labels have currently an angle < -90
-    mutate(hjust = ifelse( angle < -90, 1, 0)) %>%
-    # Flip angles BY 180 degrees to make them readable
-    mutate(angle=ifelse(angle < -90, angle+180, angle))
-  y_max <- ceiling(max(data$xp))
-  
-  p <- ggplot(data) + 
-    geom_col(aes(x = site, y = xp), fill="steelblue3") +
-    coord_polar(start = 0) +
-    ylim(-ceiling(y_max/3),y_max) +
-    ylab(label="latent community expression, %") +
-    ggtitle(paste("Site expression of latent community", .x, sep = " ")) +
-    theme(
-      axis.text.x = element_blank(),
-      axis.title.x = element_blank())
-  # Add the survey labels.
-  plot(p + geom_text(data = data_labels, aes(x=id, y=ceiling(0.8*y_max), label=site, hjust=hjust), 
-                color="black", alpha=0.7, size=3, angle=data_labels$angle, inherit.aes = FALSE ))
-})
-
-### EXTRAS ################
-
-# Extract dyads
-nodes <- g1 %>% activate(nodes) %>% as_tibble()
-
-dyads <- g1 %>% activate(edges) %>%
-  as_tibble() %>%
-  filter(!is.na(edge_latent_community)) %>%
-  select(from, to, sgn, edge_latent_community)
-
-dyads <- dyads %>%
-  mutate(A = nodes$name[from]) %>%
-  mutate(B = nodes$name[to])
-
-dyads <- dyads %>% select(A, B, sgn, edge_latent_community) %>%
-  rename(sign = sgn, community = edge_latent_community)
-
-write.csv(lc_stats, "lc_stats.csv")
-write.csv(dyads, "dyads.csv")
-
-
+# polar_plot_list <- map(.x = lc_stats$lc, .f = ~{
+#   data <- site_xp %>% filter(lc == .x)
+#   data_labels <- data %>% 
+#     select(site) %>% 
+#     mutate(id = seq_along(site)) %>% 
+#     # Subtract 0.5 because the letter must have the angle of the center of the bars, 
+#     # not extreme right(1) or extreme left (0)
+#     mutate(angle =  90 - 360 * (id-0.5) /length(site)) %>%  
+#     # calculate the alignment of labels: right or left
+#     # If I am on the left part of the plot, my labels have currently an angle < -90
+#     mutate(hjust = ifelse( angle < -90, 1, 0)) %>%
+#     # Flip angles BY 180 degrees to make them readable
+#     mutate(angle=ifelse(angle < -90, angle+180, angle))
+#   y_max <- ceiling(max(data$xp))
+#   
+#   p <- ggplot(data) + 
+#     geom_col(aes(x = site, y = xp), fill="steelblue3") +
+#     coord_polar(start = 0) +
+#     ylim(-ceiling(y_max/3),y_max) +
+#     ylab(label="latent community expression, %") +
+#     ggtitle(paste("Site expression of latent community", .x, sep = " ")) +
+#     theme(
+#       axis.text.x = element_blank(),
+#       axis.title.x = element_blank())
+#   # Add the survey labels.
+#   plot(p + geom_text(data = data_labels, aes(x=id, y=ceiling(0.8*y_max), label=site, hjust=hjust), 
+#                 color="black", alpha=0.7, size=3, angle=data_labels$angle, inherit.aes = FALSE ))
+# })
+# 
+# ### EXTRAS ################
+# 
+# # Extract dyads
+# nodes <- g1 %>% activate(nodes) %>% as_tibble()
+# 
+# dyads <- g1 %>% activate(edges) %>%
+#   as_tibble() %>%
+#   filter(!is.na(edge_latent_community)) %>%
+#   select(from, to, sgn, edge_latent_community)
+# 
+# dyads <- dyads %>%
+#   mutate(A = nodes$name[from]) %>%
+#   mutate(B = nodes$name[to])
+# 
+# dyads <- dyads %>% select(A, B, sgn, edge_latent_community) %>%
+#   rename(sign = sgn, community = edge_latent_community)
+# 
+# write.csv(lc_stats, "lc_stats.csv")
+# write.csv(dyads, "dyads.csv")
+# 
+# 
