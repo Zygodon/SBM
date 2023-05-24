63a7d9c531092227a4e7f345ac2580cdf5440372
Zygodon <j.b.pilkington@gmail.com>
2023-05-24 21:10:49 +0100

    Working on identifying associates

--- work.R
+++ work.R
@@ -1,227 +1,51 @@
 
-# libraries #########################
-library("RMySQL")
-library(tidyverse)
-library(igraph)
-library(ggraph)
-library(tidygraph)
-library(RColorBrewer)
-library(sbm)
+edges <- g1 |> activate(edges) |> select (A, B, edge_latent_community) |> as_tibble()
+out_edges <- edges |> filter(is.na(edge_latent_community))
 
-# Functions #######################
-dbDisconnectAll <- function(){
-  ile <- length(dbListConnections(MySQL())  )
-  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
-  cat(sprintf("%s connection(s) closed.\n", ile))
+EdgeEnd <- function(.x){alc <- g1 |> activate(nodes) |>
+                        filter(name == .x) |>
+                        select(latent_community) |>
+                        as_tibble()
+  return(alc$latent_community)
 }
 
-GetQuadratData <-  function()
-{
-  # GET DATA FROM DB
-  # Remote DB with password
-  con <- dbConnect(MySQL(), 
-                   user  = "guest",
-                   password    = "guest",
-                   dbname="meadows",
-                   port = 3306,
-                   host   = "sxouse.ddns.net")
-  
-  # SQL query to extract quadrat data and associated species  
-  q <- sprintf('SELECT DISTINCT quadrat_id, species_name
-  from surveys join quadrats on quadrats.survey_id = surveys.surveys_id
-  join records on quadrats.quadrats_id = records.quadrat_id
-  join species on species.species_id = records.species_id
-  where species.species_id != 4 and major_nvc_community like "MG%%" and quadrat_size = "2x2";') 
-  
-  rs1 = dbSendQuery(con, q)
-  return(as_tibble(fetch(rs1, n=-1)))
-  dbDisconnectAll()
-}
+out_edges <- out_edges |> mutate(A_lc = map_int(.x = A, EdgeEnd))
+out_edges <- out_edges |> mutate(B_lc = map_int(.x = B, EdgeEnd))
 
-### DATA WRANGLING #####################
-# GET DATA FROM DB
-# Remote DB with password
-con <- dbConnect(MySQL(), 
-                 user  = "guest",
-                 password    = "guest",
-                 dbname="meadows",
-                 port = 3306,
-                 host   = "sxouse.ddns.net")
+associates_lc1 <- out_edges |> filter((A_lc == 1)|(B_lc == 1)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
-q <- sprintf('select quadrat_id, survey_id, assembly_name, species.species_name from surveys
-      join quadrats on quadrats.survey_id = surveys_id
-      join visit_dates on quadrats.vd_id = visit_dates.vds_id
-      join records on records.quadrat_id = quadrats_id
-      join species on species.species_id = records.species_id where species.species_id != 4 and
-      major_nvc_community like "MG%%" and quadrat_size = "2x2";')
+associates_lc2 <- out_edges |> filter((A_lc == 2)|(B_lc == 2)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
-# NOTE the double %% to escape the % formatting character
+associates_lc3 <- out_edges |> filter((A_lc == 3)|(B_lc == 3)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
-rs1 = dbSendQuery(con, q)
-d <- as_tibble(fetch(rs1, n=-1))
-dbDisconnectAll()
-rm(con, q, rs1)
+associates_lc4 <- out_edges |> filter((A_lc == 4)|(B_lc == 4)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
-# Count (n) hits for each species (columns) in each quadrat (rows)
-d <- (d %>% select(quadrat_id, species_name)
-      %>% group_by(quadrat_id, species_name)
-      %>% summarise(n=n())
-      %>% ungroup()
-      %>% pivot_wider(names_from = species_name, values_from = n))
+associates_lc5 <- out_edges |> filter((A_lc == 5)|(B_lc == 5)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
-# At this point, d has the number of hits for each survey and species.
-# Replace anything numeric with 1, and any NA with 0
-d <- (d %>% select(-quadrat_id) %>% replace(., !is.na(.), 1)
-      %>% replace(., is.na(.), 0)) # Replace NAs with 0)
-n_sites <- d %>% select(1) %>% count() %>% unlist()
+associates_lc6 <- out_edges |> filter((A_lc == 6)|(B_lc == 6)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
-# write_csv(d, "hits.csv", col_names = TRUE)
-
-# What follows thanks to Brian Shalloway: 
-# https://www.bryanshalloway.com/2020/06/03/tidy-2-way-column-combinations/#fn4
-# Manipulating the data into contingency tables for species pairs (dyads)
-# and getting the fisher.test statisics using map not for-loops.
-df_lists <- d %>%
-  summarise_all(list) %>% 
-  pivot_longer(cols = everything(), 
-               names_to = "var", 
-               values_to = "vector")
-
-df_lists_comb <- expand(df_lists,
-                        nesting(var, vector),
-                        nesting(var2 = var, vector2 = vector))
-
-is_event <- function(...){ # See map2_int below
-  sum(..1 & ..2)
-}
-
-df_lists_comb_as <- df_lists_comb %>% 
-  filter(var != var2) %>% 
-  mutate(events = map2_int(.x = vector, .y = vector2, .f = is_event)) %>% 
-  filter((events > 0) & events < length(df_lists$vector[[1]])) %>%
-  select(-events)
-rm(is_event)
-
-# function to make the contingency tables.
-xtab <- function(...){ # See map2 below
-  t <- table(..1, ..2) 
-}
-# And map them into a convenient data structure.
-df_tables <- df_lists_comb_as %>% 
-  mutate(xtable = map2(vector, vector2, xtab))
-# Clean up...
-rm(df_lists, df_lists_comb, df_lists_comb_as, xtab)
-
-# Make a graph and simplify it before doing the fisher test
-df_edges <- df_tables %>% select(var, var2) %>% rename(from = var, to = var2)
-g1 <- as_tbl_graph(graph.data.frame(d = df_edges, directed = FALSE))
-
-# Add edge attributes to g1 edges
-g1 <- g1 %>% activate("edges") %>%
-  mutate(xtable = df_tables$xtable)
-
-# Add Observed dyad probabilities 
-g1 <- g1 %>% activate("edges") %>%
-  mutate(p_obs = map_dbl(.x = xtable, .f = ~{unlist(.x[4])/sum(unlist(.x))}))
-
-# Simplify the Tidygraph way
-g1 <- g1 %>% activate("edges") %>% 
-  filter(!edge_is_multiple()) %>%
-  filter(!edge_is_loop())
-# Clean up ...
-# [1,1rm(df_edges, df_tables)
-
-# Important function to SAFELY apply fisher.test, ensuring we get
-# some return value for dyad.
-sft <- safely(.f = fisher.test, otherwise = NULL, quiet = TRUE)
-
-# Map fisher.test over dyads
-g1 <- g1 %>% activate("edges") %>%
-  mutate(f_test = map(xtable, sft))
-rm(sft)
-
-# Temporary data structures needed to extract fisher.test statistics
-# from <hresult> class and transfer them to edges data where they belong.
-x <- tibble(pval = rep(0, gsize(g1)))
-x <- x %>% mutate(lor = pval)
-df_t <- g1 %>% activate("edges") %>% as_tibble()
-
-# Extract p.val and Odds Ratio from fisher.test <hresult>
-# Would prefer to do using map.
-for(i in 1:gsize(g1)){ 
-  x$pval[i] <- (ifelse(is.null(df_t[["f_test"]][[i]][["result"]][["p.value"]]), 100, df_t[["f_test"]][[i]][["result"]][["p.value"]]))
-  x$lor[i] <- (ifelse(is.null(df_t[["f_test"]][[i]][["result"]][["p.value"]]), 100, df_t[["f_test"]][[i]][["result"]][["estimate"]]))
-}
-# Clean up...
-rm(i, df_t)
-
-# Actually transfer the stats to the edge attributes
-g1 <- g1 %>%
-  activate("edges") %>%
-  mutate(pval = x$pval) %>% 
-  mutate(lor = log(x$lor)) # NOTE log
-# Clean up ...  
-rm(x)
-
-### KEY STEP IN ANALYISIS. ########
-# Select filters on pval and lor
-# IMPORTANT NOTE SBM is told nothing about pval or lor. 
-# They are used only to select dyads for the SBM to work on.
-g1 <- g1 %>%
-  activate("edges") %>%
-  filter(pval < 0.05) %>% 
-  filter(is.finite(lor)) %>%
-  mutate(weight = abs(lor)) %>%
-  mutate(sgn = ifelse(lor > 0, "associative", "dissociative"))
-# Remove isolated nodes
-g1 <- g1 %>% activate("nodes") %>% filter(degree(g1) > 0)
-
-# Save raw g1 here BEFORE adding stuff from the_model
-write_rds(g1, "Qg1.rds")
-
-# Check on the lor histogram
-edges <- g1 %>% activate(edges) %>% as_tibble()
-plt1 <- ggplot(edges, aes(lor))  +
-  geom_histogram(aes(y = ..density..), binwidth = 0.25, colour = "black") +
-  geom_vline(xintercept = 0, colour = "red") +
-  xlim(-5, 10) +
-  ylim(0.0, 0.4) +
-  labs(title ="", x = "log(odds ratio)")
-plot(plt1)
-
-# Obtain the adjacency matrix ...
-M <- as_adj(g1, type = "both", sparse = F)
-# and covariate matrix if needed
-P <- as_adj(g1, attr = "p_obs", type = "both", sparse = F)
-L <- as_adj(g1, attr = "lor", type = "both", sparse = F)
+associates_lc7 <- out_edges |> filter((A_lc == 7)|(B_lc == 7)) |>
+  select(A, B) |>
+  pivot_longer(c(A,B)) |>
+  distinct(value)
 
 
-quadrat_data <- GetQuadratData() %>%
-  rename(species = species_name)
 
-nq <- quadrat_data %>% select(quadrat_id) %>% distinct() %>% count() %>% as.integer()
-
-hits <- gather(d) %>% 
-  group_by(key) %>% 
-  summarise(count = sum(value)) %>% 
-  mutate(frequency = 100*count/nq) %>%
-  rename(species = key)
-# count: how many quadrats the species was found in.
-
-# In g1, add count and frequency to nodes
-g1 <- g1 %>% activate(nodes) %>% left_join(hits, join_by(name == species))
-theta <- seq(0, 70, by=10)
-
-# And finally, build the model!
-# No covariate
-# the_model <- estimateSimpleSBM(M, 'bernoulli', estimOptions = list(plot = F )) #TRUE))
-# With covariate
-
-model_list <- map(.x = theta, .f = ~{
-  g1 <- g1 %>%
-    activate("edges") %>%
-    filter(frequency > .x) %>% 
-    
-    the_model <- estimateSimpleSBM(M, 'bernoulli', covariates = list(P), estimOptions = list(plot =  F)) #TRUE))
-    # the_model <- estimateSimpleSBM(M, 'bernoulli', covariates = list(L), estimOptions = list(plot =  T)) #TRUE))
-})
