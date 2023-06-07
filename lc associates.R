712f81b7fb224a89d4254ea5e8aafdfce4fde505
Zygodon <j.b.pilkington@gmail.com>
2023-06-07 16:25:07 +0100

    Explanatory comments added.

--- lc associates.R
+++ lc associates.R
@@ -1,7 +1,13 @@
+# List the associate species for each LC (as opposed to the prototypes).
 
 edges <- g1 |> activate(edges) |> select (A, B, edge_latent_community) |> as_tibble()
-out_edges <- edges |> filter(is.na(edge_latent_community))
+# Edges with edge_latent_community == NA are inter-block.
+out_edges <- edges |> filter(is.na(edge_latent_community)) # - at this point we have
+# the associates, but we don't yet know which LC they are associated with ...
 
+# Function to return the LC containing the A or B end of a dyad.
+# All species are prototypes for one LC, except in the case that there is an empty block (LC7) -
+# (so the associates of LC7 are not prototypes for any community).
 EdgeEnd <- function(.x){alc <- g1 |> activate(nodes) |>
                         filter(name == .x) |>
                         select(latent_community) |>
@@ -9,9 +15,11 @@ EdgeEnd <- function(.x){alc <- g1 |> activate(nodes) |>
   return(alc$latent_community)
 }
 
+# Add the A-end and B-end LC:
 out_edges <- out_edges |> mutate(A_lc = map_int(.x = A, EdgeEnd))
 out_edges <- out_edges |> mutate(B_lc = map_int(.x = B, EdgeEnd))
 
+# Now we're in a position to find which LC the species (nodes) are associated with:
 associates_lc1 <- out_edges |> filter((A_lc == 1)|(B_lc == 1)) |>
   select(A, B) |>
   pivot_longer(c(A,B)) |>
