7f6309cd3397b4a315c8fcbeb5dc87d2ab9d5987
Zygodon <j.b.pilkington@gmail.com>
2023-06-26 15:58:47 +0100

    boxplot reshuffled simulatin + observed values for balanced/unbalanced triangles.

--- signed_nets.R
+++ signed_nets.R
@@ -6,6 +6,7 @@ library(graphlayouts)
 library(ggraph)
 library(concaveman)
 library(ggforce)
+library(tidyverse)
 
 g0 <- readRDS("~/SBM/Qg0.rds")
 g1 <- g0 |> 
@@ -14,9 +15,9 @@ g1 <- g0 |>
   activate(nodes)|>
   filter(count>99)
 
-balance_score(g1, method = "frustration")
+# balance_score(g1, method = "frustration")
 
-count_signed_triangles(g1)
+g1_triangles <- count_signed_triangles(g1)|>as_tibble()|>mutate(type=c('+++', '++-', '+--', '---'))
 clu <- signed_blockmodel(g1, k = 3, alpha = 0.9, annealing = TRUE)
 table(clu$membership)
 g1<-g1|>activate(nodes)|>mutate(tribe=clu$membership)
@@ -37,3 +38,29 @@ ggraph(g1, layout = "manual", x = bb$xy[, 1], y = bb$xy[, 2]) +
   theme_graph() #+
 
 ggblock(g1, clu$membership, show_blocks = TRUE, show_labels = TRUE)
+
+
+sgn <- g1 |> activate(edges)|>select(sign)|>as_tibble()
+smpl <- sgn|>pluck(3)|>sample() #shuffle
+g2 <- g1|>activate(edges)|>select(-sign)|>mutate(sign=smpl)
+
+# cst <- count_signed_triangles(g2)
+
+x <- seq(1:1000)
+cst <- function(x){
+  sgn <- g1 |> activate(edges)|>select(sign)|>as_tibble()
+  smpl <- sgn|>pluck(3)|>sample() #shuffle
+  g2 <- g1|>activate(edges)|>select(-sign)|>mutate(sign=smpl)
+  return(count_signed_triangles(g2))
+}
+st <- map_df(x, cst)
+summary(st)
+piv_st <- st |> pivot_longer(cols=c(1,2,3,4), names_to = "type")|> 
+
+(plot1 <- ggplot(piv_st, aes(type, value)) + 
+    geom_boxplot() +
+    geom_point(data = g1_triangles, colour='red', pch=8, size=10)
+)
+
+plot(plot1)
+
