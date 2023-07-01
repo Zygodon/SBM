c4b3c2f9cda63ddd58bcbbff09ee37a2688ce206
Zygodon <j.b.pilkington@gmail.com>
2023-07-01 20:13:29 +0100

    Improved balance plot.

--- signed_nets.R
+++ signed_nets.R
@@ -1,3 +1,4 @@
+# INVESTIGATIONS WITH PACKAGE SIGNNET.
 
 library(signnet)
 library(igraph)
@@ -7,6 +8,7 @@ library(ggraph)
 library(concaveman)
 library(ggforce)
 library(tidyverse)
+library(colorspace)
 
 g0 <- readRDS("~/SBM/Qg0.rds")
 g1 <- g0 |> 
@@ -15,10 +17,41 @@ g1 <- g0 |>
   activate(nodes)|>
   filter(count>99)
 
+### EXAMINE SIGNED TRIANGLES ################
+# NOTE: prior to and independent of any models.
 # balance_score(g1, method = "frustration")
-
 g1_triangles <- count_signed_triangles(g1)|>as_tibble()|>mutate(type=c('+++', '++-', '+--', '---'))
+g1_triangles <- g1_triangles|>mutate(balance=ifelse(type %in% c('+++', '+--'), 'balanced', 'unbalanced'))
 
+# Shuffle + and - edges to compare distribution with observed counts (g1_triangles)
+# sgn <- g1 |> activate(edges)|>select(sign)|>as_tibble()
+# smpl <- sgn|>pluck(3)|>sample() # shuffle
+# g2 <- g1|>activate(edges)|>select(-sign)|>mutate(sign=smpl)
+
+x <- seq(1:1000)
+ShuffleSign <- function(x){
+  sgn <- g1 |> activate(edges)|>select(sign)|>as_tibble()
+  smpl <- sgn|>pluck(3)|>sample() #shuffle
+  g2 <- g1|>activate(edges)|>select(-sign)|>mutate(sign=smpl)
+  return(count_signed_triangles(g2))
+}
+st <- map_df(x, ShuffleSign)
+summary(st)
+piv_st <- st |> pivot_longer(cols=c(1,2,3,4), names_to = "type") 
+piv_st <- piv_st|>mutate(balance=ifelse(type %in% c('+++', '+--'), 'balanced', 'unbalanced'))
+
+# This is perhaps the most convincing evidence for intransitive competition.
+plot1 <- ggplot(piv_st, aes(type, value, fill=balance)) + 
+  geom_boxplot(alpha=0.5) +
+  geom_point(data = g1_triangles, aes(x = type, y = value, colour=balance), pch=19, size=10, alpha=0.8)  +
+  geom_point(data = g1_triangles, aes(x = type, y = value), colour = 'black', pch=8, size=3) +
+  scale_colour_discrete_qualitative() +
+  scale_fill_discrete_qualitative() 
+  # scale_colour_brewer(palette = "Dark2") +
+  # scale_fill_brewer(palette = "Dark2")
+plot(plot1)
+
+# MAKE SIGNED BLOCKMODEL ##########################
 # signed blockmodel from package signnet. High value for alpha penalises in_block positives.
 # clu <- signed_blockmodel(g1, k = 3, alpha = 0.9, annealing = TRUE)
 clu <- signed_blockmodel(g1, k = 3, alpha = 0.9, annealing = TRUE)
@@ -42,25 +75,4 @@ plot(ggraph(g1, layout = "manual", x = bb$xy[, 1], y = bb$xy[, 2]) +
 
 plot(ggblock(g1, clu$membership, show_blocks = TRUE, show_labels = TRUE))
 
-# Shuffle + and - edges to compare distribution with observed counts (g1_triangles)
-sgn <- g1 |> activate(edges)|>select(sign)|>as_tibble()
-smpl <- sgn|>pluck(3)|>sample() #shuffle
-g2 <- g1|>activate(edges)|>select(-sign)|>mutate(sign=smpl)
-
-x <- seq(1:1000)
-ShuffleSign <- function(x){
-  sgn <- g1 |> activate(edges)|>select(sign)|>as_tibble()
-  smpl <- sgn|>pluck(3)|>sample() #shuffle
-  g2 <- g1|>activate(edges)|>select(-sign)|>mutate(sign=smpl)
-  return(count_signed_triangles(g2))
-}
-st <- map_df(x, ShuffleSign)
-summary(st)
-piv_st <- st |> pivot_longer(cols=c(1,2,3,4), names_to = "type") 
-
-plot1 <- ggplot(piv_st, aes(type, value)) + 
-    geom_boxplot() +
-    geom_point(data = g1_triangles, colour='red', pch=8, size=10)
-
-plot(plot1)
 
